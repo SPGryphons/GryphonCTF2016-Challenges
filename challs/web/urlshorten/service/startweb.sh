b72ad55f4c6066057e6e2f855f4ae389ee7ab3d1
pc84560895 <admin@chenqiurong.com>
2016-09-12 01:59:55 +0800

    Modify script for urlshortener

--- challs/web/urlshorten/service/startweb.sh
+++ challs/web/urlshorten/service/startweb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
-docker run -d --link db-urlshorten --name urlshorten -v ${DIR}:/usr/local/tomcat/webapps tomcat:8-jre8
+docker run -d -p 9997:8080 --link db-urlshorten --name urlshorten -v ${DIR}:/usr/local/tomcat/webapps tomcat:8-jre8
