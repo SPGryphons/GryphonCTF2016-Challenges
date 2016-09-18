c05f48385dc96c3060343b86dd08dae5bc8efd6c
Creastery <Creastery@users.noreply.github.com>
2016-09-18 22:38:45 +0800

    Proposed Fix for EzPwn (Issue #1)
    Also cleaned up the code a little.

--- challs/pwn/ezpwn/service/add.c
+++ challs/pwn/ezpwn/service/add.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-void main() {
+int main() {
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
 
     srand(time(NULL));
-    int r = rand() % 1000 + 1000;
+    int r = rand() % 999 + 1001;
     int i;
-    int tries = 2;
     char line[256];
-    
+
     printf("Welcome to my calculator!\n");
     printf("Unfortunately, I only know how to do addition :(\n");
     printf("A prize will be given if you can make the number become 1000 in 1 try!\n");
     printf("Type in a number (Currently: %d): ", r);
 
     if (fgets(line, sizeof(line), stdin)) {
-        if (line[0] == '-') {
-            line[0] = '0';
-        }
         if (1 == sscanf(line, "%d", &i)) {
-            r += i;
+            r += (strchr(line, '-') == NULL) ? i : 0;
         }
     }
 
     if (r == 1000) {
         printf("Here is your prize GCTF{%s}!\n","0v3rfl0w_b4ck_70_7h0u54nd");
+        return 0;
     } else {
         printf("Sorry, your end value is %d, not 1000. Try again!\n", r);
+        return 1;
     }
 }
